a4f43214fefdea7e5d80fdd4e34485db753f1d2b
Rantidhanty <rantidhanty@gmail.com>
2025-02-21 16:27:55 +0700

    solve bug css

--- js/script.js
+++ js/script.js
@@ -25,22 +25,37 @@ function toggleCheck() {
   }
 }
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const music = document.getElementById("bg-music");
+
+//   // Coba putar musik saat halaman dimuat
+//   music.play().catch(() => {
+//     console.log("Autoplay diblokir, menunggu interaksi pengguna...");
+//   });
+
+//   // Jika autoplay diblokir, jalankan saat pengguna berinteraksi pertama kali
+//   document.body.addEventListener(
+//     "click",
+//     function () {
+//       music.play();
+//       console.log("Musik diputar setelah interaksi pengguna.");
+//     },
+//     { once: true }
+//   ); // Hanya dijalankan sekali
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const music = document.getElementById("bg-music");
-
-  // Coba putar musik saat halaman dimuat
+  alert("Assalamu’alaikum! Selamat datang di TK Islam Harapan Sukatani 😊");
   music.play().catch(() => {
     console.log("Autoplay diblokir, menunggu interaksi pengguna...");
+    document.body.addEventListener(
+      "click",
+      function () {
+        music.play();
+        console.log("Musik diputar setelah interaksi pengguna.");
+      },
+      { once: true }
+    );
   });
-
-  // Jika autoplay diblokir, jalankan saat pengguna berinteraksi pertama kali
-  document.body.addEventListener(
-    "click",
-    function () {
-      music.play();
-      console.log("Musik diputar setelah interaksi pengguna.");
-    },
-    { once: true }
-  ); // Hanya dijalankan sekali
 });
-
